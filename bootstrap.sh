4099f22f50931c83d3088c6026cd52907a935ec1
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2018-05-06 18:29:42 +0200

    Fix back up process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+test -d ../.origdotold && rm -r ../.origdotold
 test -d ../.origdot && mv -vf ../.origdot ../.origdotold
 mkdir -vp ../.origdot/.config
 
 for file in *; do
 	echo $file
-	if [ "$file" == "README.md" ]; then
+	if [ "$file" == "README.md" ] || [ "$file" == "bootstrap.sh" ]; then
 		continue;
 	fi
 
 	if [ "$file" == "config" ]; then
 		for cfile in "$file"/*; do
-			if [ -a ../."$file" ] && ! [ -h ../."$file" ]; then
+			if [ -a ../."$cfile" ] && ! [ -h ../."$cfile" ]; then
 				mv -v ../."$cfile" ../.origdot/."$file" || exit 1
 			fi
 			ln -vfs "../.dotfiles/$cfile" ../."$cfile"
