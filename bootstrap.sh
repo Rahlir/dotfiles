fca0eaa643839c4fb9c99ad70519ad02aa5b07da
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2018-05-06 18:10:34 +0200

    Add support for folder files + err exit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+test -d ../.origdot && mv -vf ../.origdot ../.origdotold
+mkdir -vp ../.origdot/.config
+
 for file in *; do
+	echo $file
 	if [ "$file" == "README.md" ]; then
 		continue;
 	fi
-	if [ -f ../."$file" ]; then
-		if ! [ -d ../.origdot ]; then
-			mkdir ../.origdot/
+
+	if [ "$file" == "config" ]; then
+		for cfile in "$file"/*; do
+			if [ -a ../."$file" ] && ! [ -h ../."$file" ]; then
+				mv -v ../."$cfile" ../.origdot/."$file" || exit 1
+			fi
+			ln -vfs "../.dotfiles/$cfile" ../."$cfile"
+		done
+	else
+		if [ -a ../."$file" ] && ! [ -h ../."$file" ]; then
+			mv -v ../."$file" ../.origdot/ || exit 1
 		fi
-		mv ../."$file" ../.origdot/
+		ln -vfs ".dotfiles/$file" ../."$file"
 	fi
-
-	ln -vfs ".dotfiles/$file" ../."$file"
 done
-
-
