6c3c114d69571175228c5ea3874e1ae2bc01eae5
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2023-08-05 12:42:18 +0200

    fix(tmux): fix uptime script on macOS

--- tmux/plugins/tmux-rahlir-statusline/scripts/uptime.sh
+++ tmux/plugins/tmux-rahlir-statusline/scripts/uptime.sh
@@ -8,7 +8,7 @@ if uptime_raw=$(uptime -p 2>&1); then
     echo -n $uptime_raw | sed 's/up //; s/,//g; s/ days\?/d/; s/ hours\?/h/; s/ minutes\?/m/; s/ weeks\?/w/'
 elif (( $(uptime | tr ',' '\n' | wc -l) == 5 )); then
     uptime_raw=$(uptime | cut -d ',' -f1 )
-    days=$(echo $uptime_raw | sed -nE 's/^.*up ([[:digit:]]+) days.*/\1/p')
+    days=$(echo $uptime_raw | sed -nE 's/^.*up ([[:digit:]]+) day(s)?.*/\1/p')
     hours=$(echo $uptime_raw | sed -nE 's/^.* ([[:digit:]]{1,2}):[[:digit:]]{2}/\1/p')
     minutes=$(echo $uptime_raw | sed -nE 's/^.* [[:digit:]]{1,2}:([[:digit:]]{2})/\1/p' | sed 's/^0//')
     final_str=""
