1adabccb9c5376393597484a34b5cf6fd0330fe9
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2024-04-16 14:21:41 +0200

    fix(zsh): consider THEMENAME when using switch-background function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -19,10 +19,13 @@ function rsync-short() {
 
 function switch-background() {
     if [[ $SHLVL != 1 ]]; then
-        echo "You seem to be in a subshell, run switch-background from top-level shell"
+        print -P "%B%f{red}Error:%b You seem to be in a subshell, run switch-background from top-level shell."
         return 1
     elif [[ -n "$SSH_CLIENT" || -n "$SSH_TTY" ]]; then
-        echo "You seem to be connected over ssh, switch-background on your local machine"
+        print -P "%B%f{red}Error:%b You seem to be connected over ssh, switch-background on your local machine.%f"
+        return 1
+    elif [[ ! -v $THEMENAME ]]; then
+        print -P "%B%f{red}Error:%b THEMENAME variable is not set.%f"
         return 1
     fi
 
@@ -50,9 +53,9 @@ function switch-background() {
     fi
 
     [[ "$newbg" == "dark" ]] && local darkmode_bool="true" || local darkmode_bool="false"
-    [[ "$newbg" == "dark" ]] && local gtk_theme="Everforest-Dark-Borderless" || local gtk_theme="Everforest-Light-Borderless"
+    [[ "$newbg" == "dark" ]] && local gtk_theme="${(C)THEMENAME}-Dark-Borderless" || local gtk_theme="${(C)THEMENAME}-Light-Borderless"
 
-    local theme_relpath="themes/everforest_$newbg.toml"
+    local theme_relpath="themes/${THEMENAME}_$newbg.toml"
     ln -fs "$theme_relpath" "$alacritty_configdir/theme.toml" &&
         touch "$alacritty_configdir/alacritty.toml" &&
         export THEMEBG=$newbg && export BAT_THEME=gruvbox-$newbg &&
@@ -64,17 +67,17 @@ function switch-background() {
             sed -E "s/(gtk-application-prefer-dark-theme[[:space:]]?=[[:space:]]?)(true|false)/\1${darkmode_bool}/" -i $gtkconfig || \
             echo "gtk-application-prefer-dark-theme = $darkmode_bool" >> $gtkconfig
         grep -q "gtk-theme-name" $gtkconfig && \
-            sed -E "s/(gtk-theme-name[[:space:]]?=[[:space:]]?)(Everforest-(Dark|Light)-Borderless)/\1${gtk_theme}/" -i $gtkconfig || \
+            sed -E "s/(gtk-theme-name[[:space:]]?=[[:space:]]?)(${(C)THEMENAME}-(Dark|Light)-Borderless)/\1${gtk_theme}/" -i $gtkconfig || \
             echo "gtk-theme-name = $gtk_theme" >> $gtkconfig
         grep -q "gtk-icon-theme-name" $gtkconfig && \
-            sed -E "s/(gtk-icon-theme-name[[:space:]]?=[[:space:]]?)(Everforest-(Dark|Light))/\1${gtk_theme%-*}/" -i $gtkconfig || \
+            sed -E "s/(gtk-icon-theme-name[[:space:]]?=[[:space:]]?)(${(C)THEMENAME}-(Dark|Light))/\1${gtk_theme%-*}/" -i $gtkconfig || \
             echo "gtk-icon-theme-name = ${gtk_theme%-*}" >> $gtkconfig
     fi
     if [[ -d ${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/colors/ ]]; then
-        ln -fs "colors/everforest-$newbg.css" ${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/colors.css
+        ln -fs "colors/${THEMENAME}-$newbg.css" ${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/colors.css
     fi
     if [[ -d ${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/colors/ ]]; then
-        ln -fs "colors/everforest-$newbg.css" ${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/colors.css
+        ln -fs "colors/${THEMENAME}-$newbg.css" ${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/colors.css
     fi
 
     if [[ "$(uname -s)" == Linux ]]; then
@@ -83,26 +86,14 @@ function switch-background() {
             gsettings set org.gnome.desktop.interface icon-theme ${gtk_theme%-*}
             gsettings set org.gnome.desktop.interface color-scheme prefer-$newbg
         fi
-        # local wallpaper_path=$HOME/Pictures/.wallpaper
-        # ln -fs "Wallpapers/archgruv_$newbg.jpg" $wallpaper_path
-        # if [[ -f ${XDG_CONFIG_HOME:-$HOME/.config}/tofi/theme ]]; then
-        #    ln -fs "themes/gruvbox_$newbg" ${XDG_CONFIG_HOME:-$HOME/.config}/tofi/theme
-        # fi
-        # if [[ -f ${XDG_CONFIG_HOME:-$HOME/.config}/sway/swaylock.conf ]]; then
-        #    ln -fs "swaylock/gruvbox_$newbg" ${XDG_CONFIG_HOME:-$HOME/.config}/sway/swaylock.conf
-        # fi
+
         if [[ -f ${XDG_CONFIG_HOME:-$HOME/.config}/mako/config ]]; then
-            ln -fs "everforest_$newbg" ${XDG_CONFIG_HOME:-$HOME/.config}/mako/config
+            ln -fs "${THEMENAME}_$newbg" ${XDG_CONFIG_HOME:-$HOME/.config}/mako/config
             (( $+commands[makoctl] )) && makoctl reload
         fi
-        # if [[ -f ${XDG_CONFIG_HOME:-$HOME/.config}/sway/theme.conf ]]; then
-        #     ln -fs "themes/gruvbox_$newbg.conf" ${XDG_CONFIG_HOME:-$HOME/.config}/sway/theme.conf
-        #     if [[ -n "$SWAYSOCK" ]]; then
-        #         swaymsg reload && makoctl reload
-        #     fi
-        # fi
+
         if [[ -f ${XDG_CONFIG_HOME:-$HOME/.config}/hypr/theme.conf ]]; then
-            ln -fs "themes/everforest_$newbg.conf" ${XDG_CONFIG_HOME:-$HOME/.config}/hypr/theme.conf
+            ln -fs "themes/${THEMENAME}_$newbg.conf" ${XDG_CONFIG_HOME:-$HOME/.config}/hypr/theme.conf
             if hypr_instances=$(hyprctl instances 2> /dev/null); then
                 if [[ -n $hypr_instances ]]; then
                     hyprctl reload &> /dev/null
