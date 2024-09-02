0c879209f3ce19f780d93e0346077bed58373e26
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2024-09-02 16:47:16 +0200

    fix(task): refactor shift-due hooks

--- task/hooks/rahlirw/dateutils.py
+++ task/hooks/rahlirw/dateutils.py
@@ -1,7 +1,8 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone, time
 
 
 LOCAL_ZONE = datetime.now().astimezone().tzinfo
+DEFAULT_TIME = time(23, 59, 59)
 
 
 def taskdate_to_datetime(taskdate: str) -> datetime:
@@ -25,3 +26,22 @@ def timedelta_to_isostring(td: timedelta) -> str:
     if minutes:
         result_str += f"{minutes:d}M"
     return result_str
+
+
+def set_default_time(old_date: datetime) -> datetime:
+    return old_date.replace(
+        hour=DEFAULT_TIME.hour,
+        minute=DEFAULT_TIME.minute,
+        second=DEFAULT_TIME.second
+    )
+
+
+def shift_due(task):
+    due_datetime = taskdate_to_datetime(task["due"])
+    if due_datetime.time() == time(0, 0, 0):  # due time is midnight
+        shifted = set_default_time(due_datetime)
+        task["due"] = datetime_to_taskdate(shifted)
+        print(
+            "Due time shifted from "
+            f"{due_datetime.strftime('%H:%M:%S')} to {shifted.strftime('%H:%M:%S')}"
+        )
