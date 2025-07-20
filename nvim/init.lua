c3f337954929d3fcf0193c7d73cf0aba3d514346
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2025-07-20 18:31:54 +0200

    fix(nvim): add clang-format to conform config

--- nvim/init.lua
+++ nvim/init.lua
@@ -720,7 +720,8 @@ require("conform").setup({
     javascript = { "prettierd", "prettier", stop_after_first = true },
     typescript = { "prettierd", "prettier", stop_after_first = true },
     typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-    vue = { "prettierd", "prettier", stop_after_first = true }
+    vue = { "prettierd", "prettier", stop_after_first = true },
+    cpp = { "clang-format" }
   },
   default_format_opts = {
     lsp_format = "fallback"
